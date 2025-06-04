580e64c7b0dc97acbe03148c83777fdfba878de4
SindreSau <sindre_50@hotmail.com>
2025-06-04 16:27:08 +0200

    fix: updated vite to use path.resolve for fixing build on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': '/src',
-      '@assets': '/src/assets',
-      '@components': '/src/components',
-      '@layouts': '/src/layouts',
-      '@styles': '/src/styles',
-      '@utils': '/src/utils',
-      '@hooks': '/src/hooks',
-      '@schemas': '/src/schemas',
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@schemas': path.resolve(__dirname, './src/schemas'),
     },
   },
 });
