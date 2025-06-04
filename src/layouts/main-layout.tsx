f921485925ff70798a59f3f2558358f1d80622fd
SindreSau <sindre_50@hotmail.com>
2025-06-04 16:21:23 +0200

    fix: vercel build failed due to import of ./app

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -6,10 +6,16 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <header className="h-8 bg-primary/10"></header>
+
       <main className="flex-grow container mx-auto my-8 md:my-12 px-4">
         {children}
       </main>
-      <footer className="h-12 bg-primary/60"></footer>
+
+      <footer className="h-12 bg-primary/60">
+        <div className="container mx-auto flex items-center justify-center h-full">
+          © {new Date().getFullYear()} JSON Dynamic Form Editor
+        </div>
+      </footer>
     </div>
   );
 };
